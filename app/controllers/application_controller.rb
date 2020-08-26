5d559c79343763e3352a9b9eb699a22aaecfe4bc
Julien Bouland <bouland.julien@gmail.com>
2020-08-26 13:15:35 +0200

    completed_at unset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate
-    if !signed_in?
+    if !signed_in
       redirect_to new_session_path
     end
   end
 
-  def signed_in?
-    current_user.signed_in?
-  end
-
-  def signed_in_as
+  def signed_in(email)
     session[:current_email] = email
   end
 
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Todo, "#completed" do
+describe Todo, "#completed?" do
   it "returns true if completed_at is set" do
     todo = Todo.new(completed_at: Time.current)
 
@@ -11,5 +11,29 @@ describe Todo, "#completed" do
     todo = Todo.new(completed_at: nil)
 
     expect(todo).not_to be_completed
+  end
+end
+
+  describe Todo, "#completed!" do
+    it "update completed_at" do
+      todo = Todo.create!(completed_at: nil)
+
+      todo.complete!
+
+      todo.reload
+
+      expect(todo).to be_completed
+  end
+end
+
+  describe Todo, "#mark_incomplete!" do
+    it "it sets completed_at to nil" do
+      todo = Todo.create!(completed_at: Time.current)
+
+      todo.mark_incomplete!
+
+      todo.reload
+
+      expect(todo).not_to be_completed
   end
 end
